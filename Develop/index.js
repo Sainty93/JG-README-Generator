4bfb352f8f540f14c9f235dfe180fa5c901a1434
Sainty93 <jenandfloyd93@icloud.com>
2022-12-14 17:18:36 -0800

    started js code

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,25 @@
-// Pkgs for app development
+// TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require('generate-markdown');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: 'title',
+        message: 'What is the title?',
+        validate: (titleInput) => titleInput
+        {
+        if (titleInput) {
+            return true;
+        } else {
+            return 'Please select a title.';
+        }
+
+        },
+    },
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (!license) === 'MIT') {
-    return '!{'
-  }
-}
+function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
