db2280aba664f25afa618de7799131d514fd3554
Sainty93 <jenandfloyd93@icloud.com>
2022-12-15 17:48:54 -0800

    started to add the license functions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if   (license === 'Apache version 2.0') {
+    return '![License: Apache version 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+}
+if (license === 'MIT') {
+  return '![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)';
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
