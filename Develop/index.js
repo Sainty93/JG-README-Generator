a8a906399a7a9d4a8597715f0a0190aa12f78a79
Sainty93 <jenandfloyd93@icloud.com>
2023-05-03 13:29:32 -0700

    went back to correct error

--- Develop/index.js
+++ Develop/index.js
@@ -1,97 +1,143 @@
-// TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
+// Array of questions for the user to input
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title?',
-        validate: (titleInput) => {
-            if (titleInput) {
-            return true;
-        } else {
-            return 'Please select a title.';
-             }
-        },
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        return 'Please input a project title.';
+      }
     },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Give a description explaining the what, why and how of project. Use the following questions as a guide:\n
-        - What was your motivation?
-        - Why did you build this project?
-        - What problem does it solve?
-        - What did you learn?\n',
-            validate: (descriptionInput) => {
-                if (descriptionInput) {
-                    return true;
-                } else {
-                    return 'Please select a description.';
-                }
-            },
-        },
-        {
-            type: 'input',
-            name: 'table of contents',
-            message: 'Table of contents',
-        },
-        {
-            type: 'input',
-            name: 'Installation',
-            message: 'Please provide installation information for the user:',
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Please provide instructions on how to use the project:',
-        },
-        {
-            type: 'list',
-            name: 'license',
-            massage: 'Please select which license you would liketo use:',
-            choices: ['Apache 2.0', 'MIT', 'GNU GPL v3.0', 'None'],
-        },
-        {
-            type: 'input',
-            name: 'contributing',
-            message: 'How can developers contribute to your project?',
-        },
-        {
-            type: 'input',
-            name: 'test',
-            message: 'How can this project be tested?',
-        },
-        {
-            type: 'input',
-            name: 'questions',
-            message: 'Enter Github username',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Enter email address:',
-            validate: (questionInput) => {
-                return true;
-            } else {
-                return 'Please provide your github username.';
-            }
-        },
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a short desciption of your project:',
+    validate: (descInput) => {
+      if (descInput) {
+        return true;
+      } else {
+        return 'Please input a project description.';
+      }
     },
+  },
+  {
+    type: 'input',
+    name: 'Table of Contents',
+    message: 'Table of Contents:',
+    validate: (tocInput) => {
+      if (tocInput) {
+        return true;
+      } else {
+        return 'Please input a Table of Contents for your project.';
+      }
+    },
+  },
+  {
+    type: 'input',
+    name: 'Installation',
+    message: 'Please provide installation instructions for the user.',
+    validate: (installInput) => {
+      if (installInput) {
+        return true;
+      } else {
+        return 'Please input installation instructions.';
+      }
+    },
+  },
+  {
+    type: 'input',
+    name: 'Usage',
+    message: 'Please provide instructions on how to use your project.',
+    validate: (usageInput) => {
+      if (usageInput) {
+        return true;
+      } else {
+        return 'Please input instructions on how to use your project.';
+      }
+    },
+  },
+  {
+    type: 'list',
+    name: 'License',
+    message: 'Please select which license you would like to use.',
+    choices: [
+      'Apache License 2.0',
+      'MIT License',
+      'GNU GPL v3.0',
+      'Mozilla Public License 2.0',
+      'Eclipse Public License 2.0',
+      'None',
+    ],
+    validate: (licenseInput) => {
+      if (licenseInput) {
+        return true;
+      } else {
+        return 'Please select a license.';
+      }
+    },
+  },
+  {
+    type: 'input',
+    name: 'Contributing',
+    message: 'How can fellow developers contribute to your project?',
+    validate: (contributeInput) => {
+      if (contributeInput) {
+        return true;
+      } else {
+        return 'Please provide guidelines for contributiing to this project if applicable.';
+      }
+    },
+  },
+  {
+    type: 'input',
+    name: 'Test',
+    message: 'How can test be run for this project?',
+    validate: (testInput) => {
+      if (testInput) {
+        return true;
+      } else {
+        return 'Please provide instructions on how to test your project.';
+      }
+    },
+  },
+  {
+    type: 'input',
+    name: 'Questions',
+    message: 'Enter Github username for any inquiries about the project.',
+    validate: (questionInput) => {
+      if (questionInput) {
+        return true;
+      } else {
+        return 'Please provide your github username.';
+      }
+    },
+  },
 ];
 
+// Function used to write the README file
+function writeToFile(filename, data) {
+  fs.writeFile(filename, data, (error) => {
+    console.log(filename, data);
+    if (error) throw error;
+    console.log('Your README file has been generated');
+  });
+}
 
+// Function used to initialize the App
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    writeToFile('README.md', generateMarkdown(data));
+    console.log(data);
+  });
+}
 
-            
-
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
+// Call function to initialize App
 init();
